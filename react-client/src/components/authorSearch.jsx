82e831375c9d8e76a57a7f2f982cb10441ba95e5
Abdulsalam Shahlol <abdulsalamshahlol@gmail.com>
2020-01-02 15:43:44 +0100

    converted authorSearch to stateful component and added methods on submit and input events

--- react-client/src/components/authorSearch.jsx
+++ react-client/src/components/authorSearch.jsx
@@ -1,19 +1,55 @@
 import React from 'react';
 
-const AuthorSearch = () => (
-    <div className="container">
-        <div className="row">
-            <div className="col mt-3">
-                <form action="#" method="POST">
-                    <div className="form-group">
-                        <label>Author Name</label>
-                        <input type="text" className="form-control" placeholder="Name" />
-                        <button className="btn btn-primary">Search</button>
+class AuthorSearch extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            userId: null,
+            posts: [],
+            query: null
+        }
+    }
+
+    updateQuery(e) {
+        this.setState({
+            query: e.target.value
+        });
+    }
+
+    getPosts(event) {
+        event.preventDefault();
+        console.log(232)
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col mt-3">
+                        <form action="#" method="POST" onSubmit={(e) => { this.getPosts(e) }}>
+                            <label>Author Name</label>
+                            <div className="input-group">
+                                <input type="text" className="form-control" placeholder="Name" onInput={(e) => { this.updateQuery(e) }} />
+                                <div className="input-group-append">
+                                    <button className="btn btn-primary">Search</button>
+                                </div>
+                            </div>
+                        </form>
                     </div>
-                </form>
+                </div>
+                {
+                    this.state.userId &&
+                    <div className="row">
+                        <div className="col">
+                            <h6>Showing results for {this.state.userId}</h6>
+                        </div>
+                    </div>
+                }
+                {this.state.posts}
             </div>
-        </div>
-    </div>
-);
+        );
+    }
+}
+
 
 export default AuthorSearch;
