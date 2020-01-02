01d39b127c4e59bcd35d4e5d6beb5a5fc5621426
Abdulsalam Shahlol <abdulsalamshahlol@gmail.com>
2020-01-02 10:35:48 +0100

    defined schema for db

--- seed.js
+++ seed.js
@@ -1,11 +1,36 @@
 var data = require('./seed_data.js');
 var mongoose = require('mongoose');
 var Stories = require('./db/models/story.js');
+const fakeNews = require('./seed_data');
 
 mongoose.connect('mongodb://localhost/hackednews');
 
-var seedDb = function(data) {
+// Defining the table schema
+let bySchema = new mongoose.schema({
+  "about": { type: String },
+  "created": { type: Number },
+  "id": String,
+  "karma": Number,
+  "submitted": [Number]
+});
+
+let articleSchema = new mongoose.schema({
+  "by": bySchema,
+  "descendants": Number,
+  "id": Number,
+  "kids": [Number],
+  "score": Number,
+  "time": Number,
+  "title": String,
+  "type": String,
+  "url": String,
+});
+
+var seedDb = function (data) {
   // your code here!
+  data.forEach((article, i) => {
+    const news = new mongoose.model('news')
+  });
 };
 
 seedDb(data);
